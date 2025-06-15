b95e24dadcdb240989bde9a96a2ceb32b27440b9
altamash444 <altamashkhanak444@gmail.com>
2025-06-15 12:35:20 +0530

    Update NewsAPI.py

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,4 +1,6 @@
 from newsapi import NewsApiClient
+import requests
+import json
 
 newsapi = NewsApiClient(api_key='d70b4ef7d25143daaef85faa81ad5eb0')
 
@@ -10,6 +12,7 @@ while True:
 	print("\nWhat news you want:")
 	for news in newstype:
 		print(news.capitalize())
+	print("'q' for custom topic")
 	print("or 'quit'")
 
 	choice = input("--> ").lower()
@@ -31,5 +34,27 @@ while True:
 			if index >= non:
 				break
 			print(f"{index+1}. {article['title']}")
+	elif choice == 'q':
+		query = input("Enter your query: ")
+		
+		
+		
+		try:
+			non = int(input("How many articles to fetch: "))
+			if non <= 0:
+				print("Invalid input, enter positive number.")
+				continue
+		except ValueError:
+			print("Invalid input, not a number.")
+			continue
+			
+		r = requests.get(f"https://newsapi.org/v2/everything?q={query}&language=en&apiKey=d70b4ef7d25143daaef85faa81ad5eb0")
+		news = json.loads(r.text)
+			
+		if 'articles' in news:
+			for index, article in enumerate(news['articles']):
+				if index >= non:
+					break
+				print(f"{index+1}. {article['title']}")
 	else:
 		print("Invalid input, try again.")
